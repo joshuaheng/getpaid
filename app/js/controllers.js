8f7fc737e986bdd0c90ddc8c533d4bf2ade90d97
Joshua <joshua.hyk@gmail.com>
2013-10-24 13:16:08 -0500

    update

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,25 +1,42 @@
 var getpaidControllers = angular.module('getpaidControllers', []);
 
+//Service which returns all the receipts of the current user
+getpaidControllers.factory('receiptDataSvc', function($http) {
+	return {
+		getReceipts: function() {
+			return $http.get('./json/mock.json').then(function(result) {
+				return result.data;
+			});
+		}
+	}
+});
+
 /* Controllers */
-
 //loads all the receipts in the main page
-getpaidControllers.controller('ReceiptListCtrl', ['$scope', '$http','$location',
-	function($scope, $http, $location) {
-	//should get the 
-	$http.get('./json/mock.json')
-       .then(function(result){
-          $scope.receipts = result.data;             
-        });
+getpaidControllers.controller('ReceiptListCtrl', ['$scope', '$location','receiptDataSvc',
+	function($scope,$location,receiptDataSvc) {
+		receiptDataSvc.getReceipts().then(function(data){
+			$scope.receipts = data;
+			console.log(data);
+		});
 
-    //to be directed to detailed receipt page
-    $scope.receiptClicked = function(path){
-    	alert(path);
-    	$location.path(path);
-    	}
+
+    //to be directed to detailed receipt page when a receipt is clicked on the main page
+    $scope.receiptClicked = function(receiptId){
+    	$location.path('/receipts/'+receiptId.toString());
+    }
 }]);
 
-getpaidControllers.controller('ReceiptDetailCtrl',['$scope','$routeParams',
-	function($scope, $routeParams){
+//changes the view to the detailed receipt view based on the receiptId
+getpaidControllers.controller('ReceiptDetailCtrl',['$scope','$routeParams', 'receiptDataSvc',
+	function($scope, $routeParams, receiptDataSvc){
+
 		$scope.receiptId = $routeParams.receiptId;
-		
+		var receiptId = $scope.receiptId;
+		console.log(receiptId);
+		receiptDataSvc.getReceipts().then(function(data){
+			$scope.receipt = data[receiptId];
+			console.log(data[receiptId].items);
+		});
+
 	}]);
