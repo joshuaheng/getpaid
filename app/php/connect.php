0b36aa89c61edd3749a3a7891341b1bc93d83ba7
Joshua <joshua.hyk@gmail.com>
2013-10-30 01:29:44 -0500

    add php

---
+++ app/php/connect.php
@@ -0,0 +1,22 @@
+<?php
+        /*
+                Basic mySQL connection to GetPaid Data base                
+        */
+        $server         = "engr-cpanel-mysql.engr.illinois.edu";
+        $username         = "heng3_test";
+        $user_password         = "password1";
+        $database                = "heng3_testgetpaid";
+        
+        // Create connection to my Cpanel database
+        $dbConnection = mysqli_connect($server, $username, $user_password, $database);
+        if(mysqli_connect_errno($dbConnection)) {
+                echo "Database connection error, please check internet connection";
+                echo "Error: could not establish database connection" . mysqli_connect_error();
+        }
+
+        if(!mysqli_set_charset($dbConnection, "utf8")) {
+                echo "Error loading characterset." . mysqli_error($dbConnection);
+                exit(1);
+
+        }                
+?>
---
+++ app/php/db_get.php
@@ -0,0 +1,30 @@
+<?php
+	require("connect.php");
+
+	//preset userID to be 1 for now(person who is logged into the app)
+	$userID = 1;
+	$outputArray = array();
+	$receipt = array();
+
+	$result = mysqli_query($dbConnection,"SELECT receiptid,storeName,total,paid FROM Receipt WHERE userId = $userID");
+
+	//each $row is an array of details from Receipts table that matches our userID
+	while($row = mysqli_fetch_array($result))
+  	{
+  		$receiptID = $row[0];
+  		//Query to get all items based on current $receiptID
+  		$itemQuery = mysqli_query($dbConnection,"SELECT name, cost FROM Item WHERE receiptId = $receiptID"); 
+
+  		$receipt['id']=$row[0];
+  		$receipt['store'] = $row[1];
+  		$receipt['amount'] = $row[2];
+  		$receipt['paid'] = $row[3];
+  		$receipt['items'] = array();
+  		while($itemRow = mysqli_fetch_array($itemQuery)){
+  			$receipt['items'][$itemRow[0]] = $itemRow[1];
+  		}
+  		array_push($outputArray,$receipt);
+  		$receipt = array();
+  	}
+  	echo json_encode($outputArray);
+?>
